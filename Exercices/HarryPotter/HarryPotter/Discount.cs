182c9508c1e30608efb5ff56f9e72c8bc69d220a
Mariama673 <157657776+Mariama673@users.noreply.github.com>
2025-09-16 09:16:43 -0400

    Update Discount.cs

--- Exercices/HarryPotter/HarryPotter/Discount.cs
+++ Exercices/HarryPotter/HarryPotter/Discount.cs
@@ -8,24 +8,28 @@ namespace HarryPotter
 {
     public class Discount
     {
+        private int nbBooks;
+        private double priceRation;
+        
         public Discount(int nb, double price)
         {
-
+          nbBooks = nb;
+            this.priceRation = priceRation;
         }
 
         public bool CanBeApplied(Basket b)
         {
-            throw new NotImplementedException();
+            b.HowManyDifferent() >= nbBooks;
         }
 
         public double Apply(double basePrice)
         {
-            throw new NotImplementedException();
+            nbBooks * basePrice * priceRation;
         }
 
         public Basket RemovePaidBooks(Basket b)
         {
-            throw new NotImplementedException();
+            b.RemoveDifferent(nbBooks);
         }
     }
 }
